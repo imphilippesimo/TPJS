6a49ae14effd0d18fe4413ea435e9dce02623d8d
imphilippesimo <philippechampion58@gmail.com>
2018-06-25 02:17:39 +0200

    btt button

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+/* 
+* Define Variables
+* Calculate the document height and set the offset to a quarter of that value
+* add event listeners for scroll ans click
+*/
+var btt = document.getElementById("back-to-top"),
+    body = document.body,
+    docElem = document.documentElement,
+    offset = 100,
+    scrollPos, docHeight;
+isFirefox = navigator.userAgent.toLowerCase().lastIndexOf("firefox") > -1;
 
-function sayHello() {
-    var myName = prompt('Quel est ton nom ?');
-    alert('Bonjour ' + myName);
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
 
+if (docHeight != undefined) {
+    offset = docHeight / 8;
 }
 
+//add scroll event listener
+window.addEventListener('scroll', function (event) {
+    scrollPos = body.scrollTop || docElem.scrollTop;
+
+    btt.className = (scrollPos > offset) ? "visible" : "";
+
+});
+
+//add event listener
+btt.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (isFirefox) {
+        docElem.scrollTop = 0;
+    } else {
+        body.scrollTop = 0;
+    }
+
+})
